b8e385b1b26a389ea27550853c745bc95db3022a
+ <+>
2023-02-01 22:03:55 -0700

    phase 1 first class functions lab complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (obj) { const newObj = [obj[0], obj[1]]; return newObj };
+const returnLastTwoDrivers = function (obj) { const lastDrivers = [obj[2], obj[3]]; return lastDrivers };
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int) {
+  return function (arg) {
+    return int * arg;
+  }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(array, func) {
+  if (func === returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(array);
+  } else if (func === returnLastTwoDrivers) {
+    return returnLastTwoDrivers(array);
+  }
+}
+
